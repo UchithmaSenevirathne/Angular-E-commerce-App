e4e4a4dce9590e7160daa72856cc34845494485f
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-12 09:59:53 +0530

    add banner images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  bannerImgs = [
+    {
+      id: 1,
+      img: '../../assets/images/banner/ef637eb93bf1a887.webp',
+    },
+    {
+      id: 2,
+      img: '../../assets/images/banner/9021283f0be266c1.webp',
+    },
+    {
+      id: 3,
+      img: '../../assets/images/banner/7dcc28ed89760319.webp',
+    },
+  ];
+
 }
