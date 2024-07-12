b52546bcb8711173c600863d14a302180772f702
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-12 11:17:12 +0530

    add data storage

--- src/app/service/data-storage.service.ts
+++ src/app/service/data-storage.service.ts
@@ -6,4 +6,18 @@ import { Injectable } from '@angular/core';
 export class DataStorageService {
 
   constructor() { }
+
+  // store data in localstorage
+  storeCartData(data:any)
+  {
+      let cartData = JSON.stringify(data);
+      localStorage.setItem('cart-data',cartData);
+  }
+
+  // get data from localStorage
+  getCartData()
+  {
+    let getData:any = localStorage.getItem('cart-data');
+    return JSON.parse(getData);
+  }
 }
