0623fb151af64aa68a85b9662ae0b51dd2573c7d
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-12 10:10:24 +0530

    add product details to home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,11 +24,28 @@ export class HomeComponent implements OnInit {
   ];
 
   getCategorisData:any;
+  getApplianceProductData:any=[];
+  getFashionProductData:any=[];
+
 
   constructor(private getData:GetDataService){}
 
   ngOnInit(): void {
     this.getCategorisData = this.getData.categoriesData;
+
+    this.getData.productData.filter((ele:any)=>{
+
+      if(ele.pdCategory == 'appliances')
+      {
+          this.getApplianceProductData.push(ele);
+      }
+      if(ele.pdCategory=='fashion')
+      {
+          this.getFashionProductData.push(ele);
+      }
+  
+      
+    });
   }
 
 
