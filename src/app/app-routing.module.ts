aa3807f01fac58e02a343c6391c2a0094841aa15
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-12 10:15:53 +0530

    import productPage Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
 const routes: Routes = [
-  {path:'', component:HomeComponent}
+  {path:'', component:HomeComponent},
+  {path:'product-page/:name',component:ProductPageComponent},
 ];
 
 @NgModule({
