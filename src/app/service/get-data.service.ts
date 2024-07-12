a56791d32fc827a3d90f94fab80a4568fdb7feaf
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-12 09:58:44 +0530

    add category data

--- src/app/service/get-data.service.ts
+++ src/app/service/get-data.service.ts
@@ -5,5 +5,35 @@ import { Injectable } from '@angular/core';
 })
 export class GetDataService {
 
+   // category data 
+   categoriesData = [
+    {
+      id: 1,
+      name: 'Fashion',
+      img: '../../assets/images/categoriesimg/fashion.webp',
+      code: 'fashion',
+    },
+    {
+      id: 2,
+      name: 'Home & Furniture',
+      img: '../../assets/images/categoriesimg/home.webp',
+      code: 'homeandfurniture',
+    },
+    {
+      id: 3,
+      name: 'Appliance',
+      img: '../../assets/images/categoriesimg/appliance.webp',
+      code: 'appliances',
+    },
+    {
+      id: 4,
+      name: 'Toys',
+      img: '../../assets/images/categoriesimg/toys.webp',
+      code: 'toys',
+    },
+  ];
+
+
   constructor() { }
+
 }
