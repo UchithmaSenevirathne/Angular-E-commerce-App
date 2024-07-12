f881832e491604fda8ff129b2e37d240a4e76135
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-12 11:25:31 +0530

    create cart count

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DataStorageService } from '../service/data-storage.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  constructor(private dataStorage:DataStorageService){}
+  
+  @Input() cartCount:number=0;
+  ngOnInit(): void {
+    var getVal = this.dataStorage.getCartData();
+    this.cartCount = getVal ? getVal.length : 0;
+  }
 
 }
